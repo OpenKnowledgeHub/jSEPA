bb5f37f0c1257f3cbc8f8f19a0175389e2e0ee45
Robert Becker <robert@rbecker.eu>
2017-01-27 14:21:14 +0100

    Changes project structure to maven. Drops dependency to obantoo. This means, that german and austrian IBANs and BICs are not validated anymore against a bank data base. Uses apache commons iban validation for ibans. Drops float currency properties in favor of BigDecimal. Adds string length check and sanitization to fields requiring it.

---
+++ src/main/java/eu/rbecker/jsepa/directdebit/DirectDebitDocument.java
@@ -0,0 +1,156 @@
+package eu.rbecker.jsepa.directdebit;
+
+import eu.rbecker.jsepa.sanitization.SepaStringSanitizer;
+import eu.rbecker.jsepa.sepa.SepaUtil;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+import javax.xml.datatype.DatatypeConfigurationException;
+
+/**
+ *
+ * @author Robert Becker
+ */
+public class DirectDebitDocument {
+
+    private String creditorBic;
+
+    private String creditorIban;
+
+    private String creditorIdentifier;
+
+    private String creditorName;
+
+    private String documentMessageId;
+
+    private final List<DirectDebitPayment> payments = new ArrayList<>();
+
+    public String toXml() throws DatatypeConfigurationException {
+        return DirectDebitDocumentBuilder.toXml(this);
+    }
+
+    public DirectDebitDocument() {
+    }
+    
+    public DirectDebitDocument(String creditorBic, String creditorIban, String creditorCreditorIdentifier, String creditorName, String documentMessageId) throws SepaValidationException {
+        setCreditorBic(creditorBic);
+        setCreditorIban(creditorIban);
+        setCreditorIdentifier(creditorCreditorIdentifier);
+        setCreditorName(creditorName);
+        setDocumentMessageId(documentMessageId);
+    }
+
+    /**
+     * Returns the due date of the first payment with the given mandate type. The due date has to be equal for all payments with the same mandate type.
+     *
+     * @param directDebitType
+     * @param mandateType
+     * @return
+     */
+    public Date getDueDateByMandateType(MandateType mandateType) {
+        for (DirectDebitPayment p : payments) {
+            if (mandateType.equals(p.getMandateType())) {
+                return p.getDirectDebitDueDate();
+            }
+        }
+        return null;
+    }
+
+    public int getNumberOfPaymentsByMandateType(MandateType mt) {
+        int result = 0;
+        for (DirectDebitPayment p : getPaymentsByMandateType(mt)) {
+            result++;
+        }
+        return result;
+    }
+
+    public BigDecimal getTotalPaymentSumOfPaymentsByMandateType(MandateType mt) {
+        BigDecimal result = new BigDecimal(0).setScale(2, RoundingMode.HALF_UP);
+        for (DirectDebitPayment p : getPaymentsByMandateType(mt)) {
+            result = result.add(p.getPaymentSum());
+        }
+        return result;
+    }
+
+    private void validatePayment(DirectDebitPayment p) throws SepaValidationException {
+        p.validate();
+
+        Date currentDueDate = getDueDateByMandateType(p.getMandateType());
+        if (currentDueDate != null && !Objects.equals(p.getDirectDebitDueDate(), currentDueDate)) {
+            throw new SepaValidationException("Due date of direct debit payment must be equal to other direct debit payments of the same mandate and direct debit type.");
+        }
+    }
+
+    Iterable<DirectDebitPayment> getPaymentsByMandateType(MandateType mandateType) {
+        List<DirectDebitPayment> result = new ArrayList<>();
+        for (DirectDebitPayment p : payments) {
+            if (mandateType.equals(p.getMandateType())) {
+                result.add(p);
+            }
+        }
+        return result;
+    }
+
+    public BigDecimal getTotalPaymentSum() {
+        BigDecimal result = new BigDecimal(0).setScale(2, RoundingMode.HALF_UP);
+        for (DirectDebitPayment p : payments) {
+            result = result.add(p.getPaymentSum());
+        }
+        return result;
+    }
+
+    public void addPayment(DirectDebitPayment payment) throws SepaValidationException {
+        validatePayment(payment);
+        payments.add(payment);
+    }
+
+    public String getCreditorBic() {
+        return creditorBic;
+    }
+
+    public final void setCreditorBic(String creditorBic) throws SepaValidationException {
+        SepaUtil.validateBic(creditorBic);
+        this.creditorBic = creditorBic;
+    }
+
+    public String getCreditorIban() {
+        return creditorIban;
+    }
+
+    public final void setCreditorIban(String creditorIban) throws SepaValidationException {
+        SepaUtil.validateIban(creditorIban);
+        this.creditorIban = creditorIban;
+    }
+
+
+    public String getCreditorIdentifier() {
+        return creditorIdentifier;
+    }
+
+    public final void setCreditorIdentifier(String creditorCreditorIdentifier) {
+        this.creditorIdentifier = creditorCreditorIdentifier;
+    }
+
+    public String getCreditorName() {
+        return creditorName;
+    }
+
+    public final void setCreditorName(String creditorName) {
+        this.creditorName = SepaStringSanitizer.of(creditorName).withMaxLength(70).sanitze();
+    }
+
+    public String getDocumentMessageId() {
+        return documentMessageId;
+    }
+
+    public final void setDocumentMessageId(String documentMessageId) {
+        this.documentMessageId = SepaStringSanitizer.of(documentMessageId).withMaxLength(35).sanitze();
+    }
+
+    public List<DirectDebitPayment> getPayments() {
+        return payments;
+    }
+}
---
+++ src/test/java/eu/rbecker/jsepa/sanitization/SepaStringSanitizerTest.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.rbecker.jsepa.sanitization;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ *
+ * @author Robert Becker <robert at rbecker.eu>
+ */
+public class SepaStringSanitizerTest {
+    
+    public SepaStringSanitizerTest() {
+    }
+
+
+    /**
+     * Test of sanitze method, of class SepaStringSanitizer.
+     */
+    @org.junit.Test
+    public void testSanitze() {
+        assertEquals("abcd", SepaStringSanitizer.of("abcd").sanitze());
+        assertEquals("äbcd", SepaStringSanitizer.of("äbcd").sanitze());
+        assertEquals("_bcd", SepaStringSanitizer.of("@bcd").sanitze());
+        // see https://www.bundesbank.de/Redaktion/DE/Downloads/Aufgaben/Unbarer_Zahlungsverkehr/technische_spezifikation_sepa_lastschriften_november_2014.pdf?__blob=publicationFile
+        String allowedChars = "a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 0 1 2 3 4 5 6 7 8 9 / - ? : ( ) . , + Ä, ä, Ö, ö, Ü, ü, ß, &, *, $, %";
+        assertEquals(allowedChars, SepaStringSanitizer.of(allowedChars).sanitze());
+        
+    }
+
+    /**
+     * Test of withMaxLength method, of class SepaStringSanitizer.
+     */
+    @Test
+    public void testWithMaxLength() {
+        assertEquals("abc", SepaStringSanitizer.of("abcd").withMaxLength(3).sanitze());
+    }
+    
+}
